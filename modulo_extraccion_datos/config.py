410a9ba9fcf0b080078d794247c4f2dbf90aa6d4
MegaloIV <matiasf.alcalde.lavado@gmail.com>
2025-09-25 12:57:57 -0500

    Mejora extraccion youtube

--- modulo_extraccion_datos/config.py
+++ modulo_extraccion_datos/config.py
@@ -1,9 +1,18 @@
 import os
 
 # Configuración general
+
+# Configuración Youtube
 API_KEY = "AIzaSyA-_S2WN--eBayGW8Xspb_CCpZfWQTvNO4"
 
-QUERY = "Ripley Peru"
-MAX_VIDEOS = 200
-MAX_COMMENTS_PER_VIDEO = 1000
+QUERIES = [
+    "Ripley Peru",
+    "Opiniones Ripley",
+    "Servicio al cliente Ripley",
+    "Experiencia Ripley"
+]
+
+MAX_VIDEOS = 10
+MAX_COMMENTS_PER_VIDEO = 10
 OUTPUT_HDFS_PATH = "/user/ripley/raw/ripley_youtube_comments.json"
+
--- modulo_extraccion_datos/youtube_client.py
+++ modulo_extraccion_datos/youtube_client.py
@@ -1,20 +1,20 @@
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from config import API_KEY, QUERY, MAX_VIDEOS, MAX_COMMENTS_PER_VIDEO
+from config import API_KEY, QUERIES, MAX_VIDEOS, MAX_COMMENTS_PER_VIDEO
 
 def get_youtube_service():
     return build("youtube", "v3", developerKey=API_KEY)
 
-def fetch_videos():
+def fetch_videos(query):
     youtube = get_youtube_service()
     videos = []
     next_page_token = None
 
     while len(videos) < MAX_VIDEOS:
         search_response = youtube.search().list(
-            q=QUERY,
+            q=query,
             part="id,snippet",
-            maxResults=min(50, MAX_VIDEOS - len(videos)),  # máximo permitido por API
+            maxResults=min(50, MAX_VIDEOS - len(videos)),
             type="video",
             pageToken=next_page_token
         ).execute()
@@ -24,15 +24,15 @@ def fetch_videos():
             title = item["snippet"]["title"]
             comments = fetch_comments(youtube, video_id)
             videos.append({
+                "query": query,   # 🔹 guardamos de qué búsqueda vino
                 "id": video_id,
                 "title": title,
                 "comments": comments
             })
 
-        # siguiente página
         next_page_token = search_response.get("nextPageToken")
         if not next_page_token:
-            break  # ya no hay más resultados
+            break
 
     return videos
 
